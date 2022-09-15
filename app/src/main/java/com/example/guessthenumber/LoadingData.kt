7b92c44624e73e68154357070d453b946f1b1c39
Toni <89582846+Noccis@users.noreply.github.com>
2022-09-15 10:32:16 +0200

    implemented viewmodelscope

--- app/src/main/java/com/example/guessthenumber/LoadingData.kt
+++ app/src/main/java/com/example/guessthenumber/LoadingData.kt
@@ -13,23 +13,13 @@ object LoadingData {
     val TAG = "tag"
 
     private val listOfRandomNumbers = mutableListOf<Int>()
-    private val myJob = Job()
-    private val myScope = CoroutineScope(Dispatchers.IO + myJob)
 
     fun createMockData(){
-
-        myScope.launch {
-            for (i in 1..200000){
-                val number = (1..1000).random()
-                listOfRandomNumbers.add(number)
-                Log.d(TAG, "createMockData: Added nr $number")
-            }
-            Log.d(TAG, "createMockData: Finished!")
+        for (i in 1..200000){
+            val number = (1..1000).random()
+            listOfRandomNumbers.add(number)
+            Log.d(TAG, "createMockData: Added nr $number")
         }
+        Log.d(TAG, "createMockData: Finished!")
     }
-
-    fun stopData(){
-        myJob.cancel()
-    }
-
 }
